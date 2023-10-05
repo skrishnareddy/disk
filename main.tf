8f352644b25f68268c2046327edff9a639aab275
skrishnareddy <120174947+skrishnareddy@users.noreply.github.com>
2023-10-05 19:27:28 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region = "${var.aws_region}"
+}
+resource "aws_vpc" "default" {
+    cidr_block = "${var.vpc_cidr}"
+    enable_dns_hostnames = true
+    tags = {
+        name = "${var.vpc_name}"
+    }
+}
